67c23e5f32e101175c3a44417717dc1d8e3c8416
Rituparna_Tithi <rituparna15-9340@diu.edu.bd>
2021-03-11 20:33:21 -0500

    new

---
+++ Practice_code/Pattern3.java
@@ -0,0 +1,25 @@
+
+public class Pattern3 {
+    
+    public static void main(String []args){
+        int n = 0;
+        for(int row = 1;row<=n;row++){
+            for(int col=1;col<=n;col++){
+                System.out.print(" ");
+                {
+                    for( col=1;col<=2*row-1;col++){
+                        System.out.print("*");
+                        
+                    }
+                    
+                }
+                System.out.println(" ");
+                
+            }
+            
+            
+        }
+        
+    
+}
+}
